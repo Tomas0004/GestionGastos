709a5416fa2b595d2c75027070efe98937d470c2
Tomas0004 <peresprueba29@gmail.com>
2025-03-07 10:43:50 -0500

    add the javafx

--- gestiongastos/src/main/java/com/test/Runner.java
+++ gestiongastos/src/main/java/com/test/Runner.java
@@ -1,7 +1,20 @@
 package com.test;
 
-public class Runner {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+public class Runner extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Gestión de gastos");
+        primaryStage.show();
+        //Scene scene = new Scene(new MainView(), 800, 600);
         
     }
 }
