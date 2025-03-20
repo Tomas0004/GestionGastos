ec97d2612c8ca5ee575bf287270e40239d22f489
Tomas0004 <peresprueba29@gmail.com>
2025-03-19 21:22:28 -0500

    app executable

--- gestiongastos/src/main/java/com/test/Runner.java
+++ gestiongastos/src/main/java/com/test/Runner.java
@@ -1,10 +1,16 @@
 package com.test;
 
-import com.model.Constants;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.Runtime.Version;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.model.*;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -14,7 +20,39 @@ public class Runner extends Application {
     Button buttonOne = new Button();
 
     public static void main(String[] args) {
-        launch(args);
+        ArrayList<Versions> usersVersions = new ArrayList<>();
+        usersVersions.add(new Versions());
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("1234", "Tomas1234", "Tomas", 100000));
+        users.getLast().addAction(new Action(LocalDate.now(), LocalTime.now(), 33333));
+        users.add(new User("0000", "Peres", "Pepito", 3333333));
+        users.getLast().addAction(new Action(LocalDate.now(), LocalTime.now(), 333333));
+
+        usersVersions.getLast().setVersions(users);
+        System.out.println(usersVersions);
+
+        try {
+            String temp = Encryption.encript(JSONHandler.ArrayListVersionsToStringJSON(usersVersions));
+
+            System.out.println(temp.length());
+            IOHandler.createFile(Constants.PATH_DEST_CREATE, temp);
+            System.out.println(Encryption.decrypt(IOHandler.readerFile(Constants.PATH_DEST_CREATE)));
+            System.out.println(JSONHandler.StringJSONToArrayListVersions(Encryption.decrypt(IOHandler.readerFile(Constants.PATH_DEST_CREATE))));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //launch(args);
+        /*try {
+            IOHandler.createFile("gestiongastos/src/main/resources/output.txt", "Hello World\nComo estan?");
+            System.out.println(IOHandler.readerFile("gestiongastos/src/main/resources/output.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        return ;
     }
 
     @Override
