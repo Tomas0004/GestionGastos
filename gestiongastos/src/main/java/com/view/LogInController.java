85026bed6d8c6575f51210a8dcd88859b18ecadb
Tomas0004 <peresprueba29@gmail.com>
2025-04-07 18:50:21 -0500

    feat: add controllers

--- gestiongastos/src/main/java/com/view/LogInController.java
+++ gestiongastos/src/main/java/com/view/LogInController.java
@@ -1,8 +1,78 @@
 package com.view;
 
-public class LogInController {
+import java.util.Arrays;
 
-    public void signIn(){
+import com.model.Versions;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+public class LogInController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Label msg;
+
+    public void logIn() {
+        clear();
+
+        try {
+            String user = username.getText();
+            String pass = password.getText();
+            if (Versions.findVersionsUser(user) == null) {
+                msg.setText("Usuario y/o contraseña incorrectos");
+                msg.setTextFill(Color.RED);
+                username.requestFocus();
+                return;
+            }else{
+                if (!Versions.findVersionsUser(user).getLast().getPassword().equals(pass)) {
+                    msg.setText("Usuario y/o contraseña incorrectos");
+                    msg.setTextFill(Color.RED);
+                    username.requestFocus();
+                    return;
+                }
+            }
+
+            PrimaryStage primaryStage = new PrimaryStage();
+            primaryStage.changeScene("Menu.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clear() {
+        msg.setText("");
+    }
+
+    public void advance() {
+        TextField[] fieldMap = {
+                username, password, null
+        };
+
+        for (Node node : fieldMap) {
+            if (node instanceof TextField) {
+                ((TextField) node).setOnKeyPressed(event -> {
+                    if (event.getCode().toString().equals("ENTER")) {
+                        Node nextNode = fieldMap[Arrays.asList(fieldMap).indexOf(node) + 1];
+                        if (nextNode != null) {
+                            nextNode.requestFocus();
+                        } else {
+                            logIn();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    public void signIn() {
         try {
             PrimaryStage primaryStage = new PrimaryStage();
             primaryStage.changeScene("SignIn.fxml");
--- gestiongastos/src/main/java/com/view/SignInController.java
+++ gestiongastos/src/main/java/com/view/SignInController.java
@@ -2,72 +2,128 @@ package com.view;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import com.model.Constants;
+import com.model.Encryption;
+import com.model.IOHandler;
+import com.model.JSONHandler;
 import com.model.User;
 import com.model.Versions;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
 public class SignInController {
     @FXML
-    TextField username, name, amountLeft;
+    private TextField username, name, amountLeft;
 
     @FXML
-    PasswordField pass;
+    private PasswordField pass;
 
     @FXML
     Label uMsg, pMsg, nMsg, aMsg;
 
+    @FXML
+    GridPane gridPane;
+
     public void signIn() {
+        clearMessages();
+
+        if (!validateFields()) {
+            return;
+        }
+
+        try {
+            int amountLeftValue = Integer.parseInt(amountLeft.getText());
+            if (Versions.findVersionsUser(username.getText()) != null) {
+                setErrorMessage(uMsg, "Seleccione otro nombre de usuario");
+                username.requestFocus();
+                return;
+            }
+
+            ArrayList<User> userList = new ArrayList<>();
+            userList.add(new User(pass.getText(), username.getText(), name.getText(), amountLeftValue));
+            Versions.addVersionsUser(userList);
+
+            IOHandler.createFile(Constants.PATH_DEST_CREATE,
+                    Encryption.encript(JSONHandler.ArrayListVersionsToStringJSON(Versions.getVersions())));
+
+            new PrimaryStage().changeScene("LogIn.fxml");
+        } catch (NumberFormatException e) {
+            setErrorMessage(aMsg, "Entrada invalida");
+        } catch (IOException e) {
+            System.err.println("Error al cambiar de escena: " + e.getMessage());
+        }
+    }
+
+    private boolean validateFields() {
+        boolean isValid = true;
+
+        if (username.getText().isEmpty()) {
+            setErrorMessage(uMsg, "Campo requerido");
+            username.requestFocus();
+            isValid = false;
+        } else if (pass.getText().isEmpty()) {
+            setErrorMessage(pMsg, "Campo requerido");
+            pass.requestFocus();
+            isValid = false;
+        } else if (name.getText().isEmpty()) {
+            setErrorMessage(nMsg, "Campo requerido");
+            name.requestFocus();
+            isValid = false;
+        } else if (amountLeft.getText().isEmpty()) {
+            setErrorMessage(aMsg, "Campo requerido");
+            amountLeft.requestFocus();
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    private void setErrorMessage(Label label, String message) {
+        label.setText(message);
+        label.textFillProperty().setValue(Color.RED);
+    }
+
+    private void clearMessages() {
         uMsg.setText("");
         pMsg.setText("");
         nMsg.setText("");
         aMsg.setText("");
+    }
 
-        String user = username.getText();
-        String password = pass.getText();
-        String nameUser = name.getText();
-        String amountLeftUser = amountLeft.getText();
+    public void advance() {
+        TextField[] fieldMap = {
+            username, pass, name, amountLeft, null
+        };
 
-        if (user.isEmpty()) {
-            uMsg.setText("Campo requerido");
-            uMsg.textFillProperty().setValue(Color.RED);
-        }else if(password.isEmpty()){
-            pMsg.setText("Campo requerido");
-            pMsg.textFillProperty().setValue(Color.RED);
-        }else if(nameUser.isEmpty()){
-            nMsg.setText("Campo requerido");
-            nMsg.textFillProperty().setValue(Color.RED);
-        }else if(amountLeftUser.isEmpty()){
-            aMsg.setText("Campo requerido");
-            aMsg.textFillProperty().setValue(Color.RED);
-        }else{
-            PrimaryStage primaryStage = new PrimaryStage();
-            try {
-                int amountLeft = Integer.parseInt(amountLeftUser);
-                Versions.addVersionsUser(new ArrayList<>());
-                Versions.getVersions().getLast().add(new User(password, user, nameUser, amountLeft));
-                primaryStage.changeScene("Login.fxml");
-            } catch (NumberFormatException e) {
-                aMsg.setText("Entrada invalida");
-            } catch (IOException e) {
-                e.printStackTrace();
+        for (Node node : fieldMap) {
+            if (node instanceof TextField) {
+                ((TextField) node).setOnKeyPressed(event -> {
+                    if (event.getCode().toString().equals("ENTER")) {
+                        Node nextNode = fieldMap[Arrays.asList(fieldMap).indexOf(node) + 1];
+                        if (nextNode != null) {
+                            nextNode.requestFocus();
+                        } else {
+                            signIn();
+                        }
+                    }
+                });
             }
         }
-
     }
 
-    public void back(){
-        PrimaryStage primaryStage = new PrimaryStage();
+    public void back() {
         try {
-            primaryStage.changeScene("Login.fxml");
+            new PrimaryStage().changeScene("Login.fxml");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error al cambiar de escena: " + e.getMessage());
         }
     }
-
 }
