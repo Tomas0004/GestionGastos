5f8ecae98d890e2d6c3274cd1fec692394c89406
Tomas0004 <peresprueba29@gmail.com>
2025-04-07 22:19:09 -0500

    feat: update constants

--- gestiongastos/src/main/java/com/model/Constants.java
+++ gestiongastos/src/main/java/com/model/Constants.java
@@ -1,7 +1,10 @@
 package com.model;
 
+import javafx.scene.image.Image;
+
 public class Constants {
     public static final int WIDTH = 1000;
     public static final int HEIGHT = 600;
     public static final String PATH_DEST_CREATE = "gestiongastos/src/main/resources/output.dat";
+    public static final Image ICON = new Image("file:gestiongastos/src/main/resources/com/view/icon.png");
 }
